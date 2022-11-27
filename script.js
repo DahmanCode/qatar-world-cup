27ac9bf34280734affb9da87a590eb31e0354cf7
Dahman <94912743+DahmanCode@users.noreply.github.com>
2022-11-27 19:00:39 +0100

    start on a challenge of world cup 2022 qatar wibsite

---
+++ script.js
@@ -0,0 +1,19 @@
+// BUTTON EFFECT
+
+const buttons = document.querySelectorAll('.btn');
+buttons.forEach(btn => {
+    btn.addEventListener('mouseover', function(e) {
+
+        let x = e.clientX - e.target.offsetLeft;
+        let y = e.clientY - e.target.offsetTop;
+
+        let ripples = document.createElement('span');
+        ripples.style.left = x + 'px';
+        ripples.style.top = y + 'px';
+        this.appendChild(ripples);
+
+        setTimeout(() => {
+            ripples.remove()
+        },700);
+    })
+})
